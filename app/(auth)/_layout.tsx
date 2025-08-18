0d4228251009b6b04b24886b4ff4763706ee8e2e
Beatrice Alfred <beatrice@Beatrices-MacBook-Air.local>
2025-08-18 11:04:37 +0300

    Set up and Folder structure  for the Rest of the App

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,16 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+import {SafeAreaView} from "react-native-safe-area-context";
+import {Slot} from "expo-router";
+
+export default function _Layout() {
+    return (
+        <SafeAreaView >
+            <View>
+                <Text>Auth_Layout</Text>
+            </View>
+            <Slot/>
+        </SafeAreaView>
+
+    )
+}
